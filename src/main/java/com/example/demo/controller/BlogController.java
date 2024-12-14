80cecac35faeef28050cbcc729874bfb93ea23c8
admin <bastad9@naver.com>
2024-12-14 20:36:17 +0900

    5주차 연습문제

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.demo.model.domain.Article;
+import com.example.demo.model.service.AddArticleRequest;
 import com.example.demo.model.service.BlogService;
 
 @Controller // 컨트롤러 어노테이션 명시
@@ -22,4 +25,16 @@ public class BlogController {
         model.addAttribute("articles", list); // 모델에 추가
         return "article_list"; // .HTML 연결
     }
+
+        @PostMapping("/api/articles")
+    public String addArticle(@ModelAttribute AddArticleRequest request) {
+        blogService.save(request);
+        return "redirect:/article_list";
+    }
+
+    @GetMapping("/favicon.ico")
+    public void favicon() {
+        //아무 작업도 하지 않음
+    }
+
 }
